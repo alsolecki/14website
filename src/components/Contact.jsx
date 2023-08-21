4a6a8876cfc22c600108346ea35f2099beb1f2ae
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-08-21 16:33:56 -0400

    added resume

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,10 +4,13 @@ const Contact = () => {
   return (
     <div className="contact">
       
+        <h3>Contact Info:</h3>
         <p>Alex Solecki</p>
         <p>Washington, DC Area</p>
-        <p>alsolecki@gmail.com</p>
-
+        <a href="mailto:alsolecki@gmail.com"><p>alsolecki@gmail.com</p></a>
+        <button>
+        <a href="./assets/Solecki Resume.pdf" download="Solecki Resume">Resume</a>
+</button>
     </div>
   )
 }
