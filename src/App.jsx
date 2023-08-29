d24df24e3af8d85315a9b1c2e77ff2768d67c495
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-08-29 15:45:09 -0400

    responsive navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import Navbar from './components/Navbar.jsx'
+
 import Footer from './components/Footer.jsx'
 import Hero from './components/Hero.jsx'
 import Contact from './components/Contact.jsx'
 import Content from './components/Content.jsx'
+import Navbar2 from './components/Navbar2.jsx'
 
 function App() {
 
   return (
     <>
-      <Navbar />
+      <Navbar2 />
       <Hero />
       <Content />
       <>
