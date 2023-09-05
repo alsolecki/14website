d12ec0cb36ae33bd23c16ba51c3877bf1c4c7879
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-09-04 20:41:10 -0400

    removed resume

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,7 @@
 import React from 'react'
 
-const PNG_FILE_URL = 'src/assets/SoleckiResume.pdf'
-
 const Contact = () => {
 
-  const downloadFileAtURL=(url)=>{
-    const fileName = url.split('/').pop()
-    const aTag = document.createElement('a')
-    aTag.href=url
-    aTag.setAttribute('download', fileName)
-    document.body.appendChild(aTag)
-    aTag.click();
-    aTag.remove();
-  };
 
 
   return (
@@ -23,9 +12,6 @@ const Contact = () => {
           <a href="mailto:alsolecki@gmail.com"><p>alsolecki@gmail.com</p></a>
           <a href="https://github.com/alsolecki"><p>github.com/alsolecki</p></a>
         </div>
-        <button onClick={()=>{downloadFileAtURL(PNG_FILE_URL)} }>Resume
-          
-        </button>
     </div>
   )
 }
