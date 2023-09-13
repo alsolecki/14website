22bcf94223ffbc294e13380aeeaf220e8b215a21
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-09-13 18:52:43 -0400

    fixed footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,8 +7,8 @@ const Contact = () => {
   return (
     <div className="contact inactive">
         <div className="expand">
-          <p>Alex Solecki</p>
-          <p>Washington DC Area</p>
+          <p className="name">Alex Solecki</p>
+          <p className="city">Washington DC Area</p>
           <a href="mailto:alsolecki@gmail.com"><p>alsolecki@gmail.com</p></a>
           <a href="https://github.com/alsolecki"><p>github.com/alsolecki</p></a>
           <a href="https://www.linkedin.com/in/alsolecki/"><p>linkedin.com/alsolecki</p></a>
