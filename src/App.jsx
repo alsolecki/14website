012d0455c163736697ab7b23e2113a8fff12126c
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-10-03 16:52:51 -0400

    started cartoon self

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Hero from './components/Hero.jsx'
 import Contact from './components/Contact.jsx'
 import Content from './components/Content.jsx'
 import Navbar2 from './components/Navbar2.jsx'
+import Cartoon from './components/Cartoon.jsx'
 
 function App() {
 
   return (
     <>
       <Navbar2 />
+      {/* <Cartoon /> */}
       <Hero />
       <Content />
       <>
---
+++ src/components/Cartoon.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Cartoon.css'
+
+const Cartoon = () => {
+  return (
+    <div className="cartoon">
+        <div className="head">
+            <div className="hair"></div>
+            <div className="eyes"></div>
+            <div className="nose"></div>
+        </div>
+        <div className="body">
+
+        </div>
+    </div>
+  )
+}
+
+export default Cartoon
