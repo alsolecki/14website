85409c2bb2ba7a29d457931b45b9facaab2db27d
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-10-17 18:46:24 -0400

    updated nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import Footer from './components/Footer.jsx'
 import Hero from './components/Hero.jsx'
 import Contact from './components/Contact.jsx'
 import Content from './components/Content.jsx'
-import Navbar2 from './components/Navbar2.jsx'
+import SiteNavBar from './components/navBar/SiteNavBar.jsx'
 import Cartoon from './components/Cartoon.jsx'
 
 function App() {
 
   return (
     <>
-      <Navbar2 />
+      <SiteNavBar />
       {/* <Cartoon /> */}
       <Hero />
       <Content />
